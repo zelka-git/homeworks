d10ff31cb4429bb314fa89eca504ce3c6a419398
anzhelika <anzelikasht@gmail.com>
2019-03-08 00:54:03 +0300

    добавлена основная часть 22 урока

---
+++ lesson-22/main_task/js/modal.js
@@ -0,0 +1,34 @@
+$(Document).ready(function(){
+  var button = $('#button');
+  var modal = $('#modal');
+  var close = $('#close');
+  var scrollUp = $('.scroll-up');
+  scrollUp.fadeOut();
+  button.on('click', function(){
+    modal.addClass('modal_active');
+  });
+
+  close.on('click', function(){
+    modal.removeClass('modal_active');
+  });
+
+  // scrollUp.on('click', function(){
+  //   $('body,html').animate({ scrollTop: 0 }, 500); 
+  // });
+  $(window).scroll(function() {
+    if($(this).scrollTop() != 0) {
+      scrollUp.fadeIn(); 
+    } else { 
+      scrollUp.fadeOut();
+    }
+    });
+     
+    scrollUp.click(function() {
+    $('body,html').animate({scrollTop:0},800);
+    });
+});
+
+// // Кнопка прокрутки
+// $(function() {
+  
+//   });
