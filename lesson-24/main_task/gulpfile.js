79cb27222841d59b08eab4f0ed77ada319b06c2e
anzhelika <anzelikasht@gmail.com>
2019-03-13 16:15:22 +0300

    24 домашнее задание

---
+++ lesson-24/main_task/gulpfile.js
@@ -0,0 +1,85 @@
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var htmlmin = require('gulp-htmlmin');
+var tinyPNG = require('gulp-tinypng-compress');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+// var jshint = require('gulp-jshint');
+
+
+gulp.task('minify-css', function(done){
+  // return gulp.src('css/*.css')
+  gulp.src('css/*.css')
+  .pipe(cleanCSS({
+    compatibility: 'ie8'
+  }))
+  .pipe(gulp.dest('../dist/css/'))
+
+  done();
+});
+
+gulp.task('move-js', function(done){
+  // return gulp.src('css/*.css')
+  gulp.src('js/*.js')
+
+  .pipe(gulp.dest('../dist/js/'))
+
+  done();
+});
+
+
+gulp.task('htmlmin', function(done){
+  // return gulp.src('*.html')
+  gulp.src('*.html')
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(gulp.dest('../dist/'))
+
+  done();
+});
+
+
+gulp.task('fonts', function(done){
+  // return gulp.src('*.html')
+  gulp.src('fonts/**/*')
+  .pipe(gulp.dest('../dist/fonts'))
+
+  done();
+});
+
+gulp.task('tinypng', function (done) {
+  gulp.src('img/**/*.{png,jpg,jpeg}')
+      .pipe(tinyPNG({
+          key: 'Tw6cDsYV2B5Sgqh3QvVswq6nMH3zQDlb'
+      }))
+      .pipe(gulp.dest('../dist/img/'));
+      done();
+});
+
+
+// gulp.task('minjs', function (done) {
+//   gulp.src('js/*.js', '!js/*.min.js')
+//       .pipe(uglify())
+//       // .pipe(rename({suffix: '.min'}))
+//       .pipe(gulp.dest('../dist/js/'));
+//       done();
+// });
+
+gulp.task('minjs', function(done){
+  gulp.src('js/*.js')
+      // .pipe(jshint())
+      // .pipe(jshint.reporter('default'))
+      .pipe(concat('file.js'))
+      .pipe(rename('file.min.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('../dist/js/'));
+      done();
+});
+
+
+
+gulp.task('default', gulp.parallel('minify-css', 'minjs', 'htmlmin', 'fonts', 'tinypng', function (done) {
+  // place code for your default task here
+  // console.log('Задача выполнена');
+  done();
+}));
---
+++ lesson-24/main_task/js/lesson.js
@@ -0,0 +1,93 @@
+ // Инициализация WOW.js
+ new WOW().init();
+    
+ $(document).ready(function(){
+   //валидация формы offer
+   $('#offer-form').validate({
+     rules: {
+       username: {
+         required: true,
+         minlength: 2,
+         maxlength: 15
+       },
+       email:{
+         required: true,
+         email: true
+       },
+       phone: {
+         required: true
+       }
+     },
+     messages:{
+       username: {
+         required: "Укажите имя",
+         minlength: jQuery.validator.format("Осталось символов: {0}"),
+         maxlength: jQuery.validator.format("Не больше {0} символов")
+       }, 
+       phone:{
+         required: "Укажите телефон"
+       }
+     }
+   });
+   // // Маска для телефона
+   // $('.phone').mask('8 (999) 999-99-99')
+
+   // Валидация формы brif
+   $('#brif-form').validate({
+     rules: {
+       username: {
+         required: true,
+         minlength: 2,
+         maxlength: 15
+       },
+       email:{
+         required: true,
+         email: true
+       },
+       phone: {
+         required: true
+       }
+     },
+     messages:{
+       username: {
+         required: "Укажите имя",
+         minlength: jQuery.validator.format("Осталось символов: {0}"),
+         maxlength: jQuery.validator.format("Не больше {0} символов")
+       },
+       email:{
+         required: "Нам нужен ваш email",
+          email: "Ваш email должен быть в формате name@domain.com"
+       }, 
+       phone:{
+         required: "Укажите телефон"
+       }
+     }
+   });
+   // Маска для телефона
+   $('.phone').mask('8 (999) 999-99-99')
+   // Скрипт слайдера
+   $('.slider').slick({
+     slidesToShow: 3,
+     slidesToScroll: 1,
+     prevArrow: $('.arrows__left'),
+     nextArrow: $('.arrows__right'),
+     responsive: [
+     {
+       breakpoint: 1200,
+       settings: {
+         slidesToShow: 2,
+         slidesToScroll: 1,
+       }
+     },
+     {
+       breakpoint: 768,
+       settings: {
+         slidesToShow: 1,
+         slidesToScroll: 1
+       }
+     }
+     ]
+
+
+   });
+ });
